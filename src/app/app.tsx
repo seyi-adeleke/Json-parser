202199d424bdc4281180bdeedea195442fe8a1eb
seyi-adeleke <adetokunbo.adeleke@andela.com>
2018-07-21 19:45:24 +0100

    added prod mode

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,10 +4,8 @@ import { App } from './components/App';
 import './app.scss';
 
 declare let module: any
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
-if (module.hot) {
+if (module.hot && process.env.NODE_ENV === 'development') {
     module.hot.accept();
  }
